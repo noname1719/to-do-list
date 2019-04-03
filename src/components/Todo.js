8ebe33f59570b872c2ddcaee5bc019c1495a1568
Reimu <yunihikokun@gmail.com>
2019-04-04 00:05:46 +0500

    ololo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -22,7 +22,8 @@ class Todo extends Component {
     handleClick() {
         this.setState({todos: [...this.state.todos, [this.state.text, this.state.id]]})
         this.setState({id: this.state.id + 1})
-        this.inputRef.current = ''
+        this.inputRef.current.value = ''
+
 
     }
 
