c347857699c1184c8ab16b1918595d3edd50f15f
Reimu <yunihikokun@gmail.com>
2019-04-07 10:46:46 +0500

    ololo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import Todo from './components/Todo'
+import FlavorForm from './components/FlavorForm'
 
 class App extends Component {
     render() {
@@ -9,6 +10,7 @@ class App extends Component {
                     <div className="card-panel teal lighten-2 center-align col s6 offset-s3">
                         <h3>to do:</h3>
                         <Todo/>
+                        <FlavorForm/>
                     </div>
                 </div>
             </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -6,7 +6,6 @@ class Todo extends Component {
         super(props)
         this.inputRef = React.createRef()
         this.state = {
-            value: 'cocount',
             id: 0,
             text: '',
             activeTasks: 0,
@@ -31,14 +30,6 @@ class Todo extends Component {
     updateData = (value) =>{
         this.setState({activeTasks: this.state.activeTasks + value})
     }
-    handleChange(event) {
-        this.setState({value: event.target.value});
-    }
-
-    handleSubmit(event) {
-        alert('Your favorite flavor is: ' + this.state.value);
-        event.preventDefault();
-    }
 
     render() {
         const todos = this.state.todos.map(todo => <Task todo={todo} updateData={this.updateData}/>)
@@ -50,20 +41,15 @@ class Todo extends Component {
                 <form>
                     <input ref={this.inputRef} id={'input'} type={'text'} onChange={this.handleChange}></input>
                     <input className={'btn'} type={'button'} value={'add todo'} onClick={this.handleClick}/>
+                    <label>
+                    <select>
+                        <option>1</option>
+                        <option>2</option>
+                        <option>3</option>
+                    </select>
+                    </label>
                 </form>
                 <p>Active tasks: {this.state.activeTasks}</p>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Pick your favorite flavor:
-                        <select value={this.state.value} onChange={this.handleChange}>
-                            <option value="grapefruit">Grapefruit</option>
-                            <option value="lime">Lime</option>
-                            <option value="coconut">Coconut</option>
-                            <option value="mango">Mango</option>
-                        </select>
-                    </label>
-                    <input type="submit" value="Submit" />
-                </form>
             </div>
         )
 
